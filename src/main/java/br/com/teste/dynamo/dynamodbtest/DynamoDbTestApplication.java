b25336a868e6d8fdf9a87c41c44d6390517629dd
romosken <rodrigo.mosken@outlook.com>
2022-09-28 19:40:18 -0300

    commit inicial

---
+++ src/main/java/br/com/teste/dynamo/dynamodbtest/DynamoDbTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.dynamo.dynamodbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoDbTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teste/dynamo/dynamodbtest/DynamoDbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.dynamo.dynamodbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
